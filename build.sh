b09a9d811df564233d8478a9a62811f01839bf8b
Gerard van Helden <gerard@van-helden.name>
2019-02-19 09:59:58 +0100

    verbose build

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -x
+set -e
 
 rm -rf ./bin/
 mkdir bin;
 
 javac $(find src -name "*.java") $(find test -name "*.java") -d bin
 cd bin && jar -cvf ./java-redis-client-$(git describe).jar $(find . -name "*.class")
-
